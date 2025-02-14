58d6d90316a7cd7fb7998fc14a9f20ef8ed4b221
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-14 09:47:20 -0300

    feat: criação do componente de Link

---
+++ src/components/Link/index.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+import { useLocation } from "react-router-dom";
+import React from "react";
+
+interface LinkProps {
+  children: ReactNode;
+  href: string;
+}
+
+export function Link({ children, href }: LinkProps) {
+  const location = useLocation();
+
+  const isActive = location.pathname === href;
+
+  return (
+    <a
+      href={href}
+      className={`font-sans font-semibold ${
+        isActive
+          ? "text-orange- underline"
+          : "text-black hover:underline hover:text-orange-"
+      } min-w-content whitespace-nowrap`}
+    >
+      {children}
+    </a>
+  );
+}
