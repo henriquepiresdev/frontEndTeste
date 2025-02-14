ec2b5cc41117320bacfdfb4278eecb1649d9a2ed
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-13 22:29:18 -0300

    feat: criando componente principal de form

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,48 @@
+import {
+  useForm,
+  FormProvider,
+  SubmitHandler,
+  FieldValues,
+  DefaultValues,
+} from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import React from "react";
+import { cn } from "../../utills/tailwind/cn";
+
+type FormProps<T extends FieldValues> = {
+  schema: z.ZodType<T>;
+  defaultValues: DefaultValues<T>;
+  onSubmit: SubmitHandler<T>;
+  children: React.ReactNode;
+  className?: string;
+};
+
+export function Form<T extends FieldValues>({
+  schema,
+  defaultValues,
+  onSubmit,
+  children,
+  className = "",
+}: FormProps<T>) {
+  const methods = useForm<T>({
+    resolver: zodResolver(schema),
+    defaultValues,
+  });
+
+  const handleSubmit = (data: T) => {
+    onSubmit(data);
+    methods.reset();
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form
+        onSubmit={methods.handleSubmit(handleSubmit)}
+        className={cn("space-y-4 p-4 border rounded-md", className)}
+      >
+        {children}
+      </form>
+    </FormProvider>
+  );
+}
