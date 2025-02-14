d7f9d5d45810dd2c4bbf9dfe22d92d65a72adc96
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-14 09:35:43 -0300

    fix: ajustes de configuracao

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         'orange-': 'rgba(236, 103, 36, 1)',
         'white-': "#FFFFFF",
+        'white-2': "#f5f5f5",
         'black-': "#000000",
+        'gray-': "#666666",
       },
       fontFamily: {
         interTest: ['Inter', 'sans-serif'],
@@ -17,4 +17,4 @@ export default {
     },
   },
   plugins: [],
-};
+});
