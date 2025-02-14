3436977f81f9068bc148bd96e879012e9e3fa195
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-13 22:31:50 -0300

    feat: criando componente do formHome com logica separada por hook

---
+++ src/components/FormHome/index.tsx
@@ -0,0 +1,35 @@
+import { formHomeSchema, UserFormData } from "../../schemas/formHome.schema";
+import { useFormInitial } from "./hooks/useFormInitial";
+import { ButtonCustom } from "../button";
+import { Input } from "../Input";
+import { Form } from "../Form";
+import React from "react";
+
+type FormHomeProps = {
+  className?: string;
+};
+
+export function FormHome({ className }: FormHomeProps) {
+  const { handleSubmit } = useFormInitial();
+
+  return (
+    <Form<UserFormData>
+      schema={formHomeSchema}
+      defaultValues={{ name: "" }}
+      onSubmit={handleSubmit}
+      className={className}
+    >
+      <Input
+        name="name"
+        placeholder="Digite seu nome"
+        className="flex justify-center items-center placeholder:text-2xl h-16 text-2xl bg-white- border-gray-400"
+      />
+      <ButtonCustom
+        type="submit"
+        className="bg-orange- text-white p-2 rounded mt-2 w-full"
+      >
+        Enviar
+      </ButtonCustom>
+    </Form>
+  );
+}
