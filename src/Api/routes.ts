3d39388aa5416f7678b0f9b8560d3efd701e37a0
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-14 09:46:16 -0300

    fix: ainda sobre configuracoes, dessa vez foi a variavel de ambiente que estava estourando erro de tipagens

---
+++ src/Api/routes.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosResponse } from "axios";
+import { PaginatedUserResponse, User } from "./@types/user";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export const createUser = async (user: User): Promise<AxiosResponse<User>> => {
+  return axios.post(`${apiUrl}/users`, user);
+};
+
+export const getUsers = async (
+  params?: Record<string, string>
+): Promise<AxiosResponse<PaginatedUserResponse>> => {
+  const queryParams = new URLSearchParams(params).toString();
+  return axios.get(`${apiUrl}/users${queryParams ? `?${queryParams}` : ""}`);
+};
+
+export const getUserById = async (id: string): Promise<AxiosResponse<User>> => {
+  return axios.get(`${apiUrl}/users/${id}`);
+};
+
+export const updateUser = async (
+  id: string,
+  user: Partial<User>
+): Promise<AxiosResponse<User>> => {
+  return axios.patch(`${apiUrl}/users/${id}`, user);
+};
+
+export const deleteUser = async (id: string): Promise<AxiosResponse<void>> => {
+  return axios.delete(`${apiUrl}/users/${id}`);
+};
