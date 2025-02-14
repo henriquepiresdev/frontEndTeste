514dc6ec4c187819ba08620511a228eb3780128f
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-14 07:59:33 -0300

    feat: criando estrutura para as rotas de api

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { UserProvider } from './context';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
