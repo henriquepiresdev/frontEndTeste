924e344cbd81c76ad7a4c1a6363f415d72f47ab3
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-14 09:47:34 -0300

    feat: criação do componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { LuMenu } from "react-icons/lu";
+import { Link } from "../Link";
+import { useUserContext } from "../../context";
+
+export function Header() {
+  const { username } = useUserContext();
+
+  return (
+    <header className="text-white p-4 h-32 md:h-24 flex justify-evenly flex-col gap-5 md:flex-row md:justify-between items-center bg-white shadow-sm">
+      <div className="w-full md:w-1/4 h-full flex md:justify-evenly">
+        <div className="w-2/5 md:w-1/3 h-full flex items-center justify-start md:justify-center">
+          <button>
+            <LuMenu className="w-6 h-6 text-gray- font-semibold" />
+          </button>
+        </div>
+        <div className="w-1/2 md:w-2/3 h-full flex justify-start">
+          <img src="/logo.svg" alt="logotipo da Teddy" />
+        </div>
+      </div>
+
+      <div className="w-full md:w-1/2 h-full flex justify-around items-center max-w-lg flex-wrap">
+        <Link href="/clients">Clientes</Link>
+        <Link href="/clients/selected">Clientes Selecionados</Link>
+        <Link href="/">Sair</Link>
+      </div>
+
+      <div className="hidden md:flex w-1/4 h-full">
+        <p className="text-black- font-medium flex items-center justify-end gap">
+          Olá, <span className="ml-1 font-semibold">{username}</span>
+        </p>
+      </div>
+    </header>
+  );
+}
