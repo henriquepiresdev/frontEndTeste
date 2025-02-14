f6e09179e3f745cf31d9d9c59ae7fce4c132c23a
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-14 09:34:56 -0300

    fix: ajustes de configuracoes de ambiente

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,25 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: 'detect' } }, // Change from '18.2' to 'detect'
+  parserOptions: {
+    ecmaVersion: 'latest', sourceType: 'module', ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  settings: { react: { version: 'detect' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    'react/react-in-jsx-scope': 'off', // Disables the import React error
+    'react/react-in-jsx-scope': 'off',
   },
 };
