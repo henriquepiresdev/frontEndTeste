f047cc95ccac66e1457e27eb469295203038a82c
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-14 09:47:51 -0300

    feat: adicionando thema do material

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from "@material-tailwind/react";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { UserProvider } from './context';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <ThemeProvider>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </ThemeProvider>
+
     </QueryClientProvider>
   </React.StrictMode>
 );
