f74fe5b677b6a699a32062c8b10516bf94aaa612
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-14 12:58:34 -0300

    feat: adicionando rota clients-selected

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Clients } from "../pages/Clients";
 import { Home } from "../pages/Home";
 import React from "react";
+import { ClientsSelected } from "../pages/ClientsSelected";
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/clients" element={<Clients />} />
+      <Route path="/clients-selected" element={<ClientsSelected />} />
     </Routes>
   </BrowserRouter>
 );
