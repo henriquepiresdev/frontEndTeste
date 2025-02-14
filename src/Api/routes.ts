6369a895831f3cd965190fb108ff6d010d19d1c7
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-14 19:24:10 -0300

    fix: atualizando rota de getUsers para receber o filtro

--- src/Api/routes.ts
+++ src/Api/routes.ts
@@ -1,19 +1,35 @@
-import axios, { AxiosResponse } from "axios";
 import { PaginatedUserResponse, User } from "./@types/user";
+import axios, { AxiosResponse } from "axios";
 
 // @ts-ignore
 const apiUrl = import.meta.env.VITE_API_URL;
 //esse bendito env. fica dando erro de tipagem
-
 export const createUser = async (user: User): Promise<AxiosResponse<User>> => {
   return axios.post(`${apiUrl}/users`, user);
 };
 
-export const getUsers = async (
-  params?: Record<string, string>
-): Promise<AxiosResponse<PaginatedUserResponse>> => {
-  const queryParams = new URLSearchParams(params).toString();
-  return axios.get(`${apiUrl}/users${queryParams ? `?${queryParams}` : ""}`);
+export const getUsers = async ({
+  page,
+  limit,
+  isSelected,
+}: {
+  page: string;
+  limit: string;
+  isSelected?: boolean;
+}): Promise<PaginatedUserResponse> => {
+  const params = new URLSearchParams();
+
+  params.append("page", page);
+  params.append("limit", limit);
+
+  if (isSelected !== undefined) {
+    params.append("isSelected", String(isSelected));
+  }
+
+  const response: AxiosResponse<PaginatedUserResponse> = await axios.get(
+    `${apiUrl}/users?${params.toString()}`
+  );
+  return response.data;
 };
 
 export const getUserById = async (id: string): Promise<AxiosResponse<User>> => {
