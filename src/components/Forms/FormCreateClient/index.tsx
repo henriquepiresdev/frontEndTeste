d5e12fc259a1886f3d45e54834cd925da6160b33
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-15 15:06:29 -0300

    feat: alterando formulario de criar cliente para o novo formulario componetizado

--- src/components/Forms/FormCreateClient/index.tsx
+++ src/components/Forms/FormCreateClient/index.tsx
@@ -1,13 +1,10 @@
 import { useFormCreateClient } from "./hooks/useFormCreateClient";
-import { ButtonCustom } from "../../Button";
-import { Input } from "../Input";
-import { Form } from "../Form";
+import { ClientForm } from "../FormClient";
 import React from "react";
 import {
   formCreateClientSchemaData,
   formCreateClientSchema,
 } from "../../../schemas/formCreateClientSchema";
-import { InputCurrency } from "../Input/InputCurrency";
 
 type FormHomeProps = {
   className?: string;
@@ -17,37 +14,13 @@ export function FormCreateClient({ className }: FormHomeProps) {
   const { handleSubmit } = useFormCreateClient();
 
   return (
-    <Form<formCreateClientSchemaData>
+    <ClientForm<formCreateClientSchemaData>
       schema={formCreateClientSchema}
       defaultValues={{ name: "", wage: "", enterprise: "" }}
       onSubmit={handleSubmit}
-      className="border-none p-3 w-full max-w-[400px]"
-    >
-      <h2 className="font-bold text-2xl">Criar Cliente:</h2>
-      <Input
-        name="name"
-        label="Nome"
-        placeholder="Digite seu nome"
-        className="flex justify-center items-center placeholder:text-lg h-16 bg-white- border-gray-400 text-lg "
-      />
-      <InputCurrency
-        name="wage"
-        label="Salário"
-        placeholder="R$ 0,00"
-        className="flex justify-center items-center placeholder:text-lg h-16  text-lg bg-white- border-gray-400"
-      />
-      <InputCurrency
-        name="enterprise"
-        label="Valor da empresa"
-        placeholder="R$ 0,00"
-        className="flex justify-center items-center placeholder:text-lg h-16  text-lg bg-white- border-gray-400 mb-2"
-      />
-      <ButtonCustom
-        type="submit"
-        className="bg-orange- p-4 text-white  rounded w-full"
-      >
-        Criar cliente
-      </ButtonCustom>
-    </Form>
+      title="Criar Cliente:"
+      buttonText="Criar cliente"
+      className={`border-none p-3 w-full max-w-[400px] ${className}`}
+    />
   );
 }
