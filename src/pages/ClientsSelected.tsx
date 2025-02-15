282818e9dec9a3568a95c407aa4bde877f490b0b
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-14 23:59:41 -0300

    fix: retirando isLoading, e isError, nao fazia sentido no momento. deve ser implementado futuramente.

--- src/pages/ClientsSelected.tsx
+++ src/pages/ClientsSelected.tsx
@@ -2,12 +2,10 @@ import { useClientSelected } from "./hooks/useClientSelected";
 import { ListCards } from "../components/ListCards";
 import { Header } from "../components/Header";
 import { When } from "../components/When";
-import React from "react";
+import React, { useEffect } from "react";
 
 export function ClientsSelected() {
-  const { data, isLoading, isError, page } = useClientSelected();
-  if (isLoading) return <div>Carregando...</div>;
-  if (isError) return <div>Não conseguir encontrar clientes</div>;
+  const { data, page } = useClientSelected();
   return (
     <div className="bg-white-2 w-full min-h-screen">
       <Header />
