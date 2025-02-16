45746bd60bd8cd0d89092167fad685cfad22b7e6
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-16 15:05:24 -0300

    fix: passando a mensagem de erro do hookform

--- src/schemas/formCreateClientSchema.ts
+++ src/schemas/formCreateClientSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const formCreateClientSchema = z.object({
   name: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
-  wage: z.string().min(5),
-  enterprise: z.string().min(5),
+  wage: z.string().min(5, "O Sálario deve ser maior que R$ 99,00"),
+  enterprise: z.string().min(5, "O Sálario deve ser maior que R$ 99,00"),
 });
 export type formCreateClientSchemaData = z.infer<typeof formCreateClientSchema>;
